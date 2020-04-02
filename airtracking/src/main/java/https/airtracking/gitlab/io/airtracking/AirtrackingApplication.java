cd89f97e63b8fac65243a56533748e1b1c4bd1c1
Diogo Mota <fc46410@alunos.fc.ul.pt>
2020-04-02 17:05:47 +0100

    spring added

---
+++ airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/AirtrackingApplication.java
@@ -0,0 +1,13 @@
+package https.airtracking.gitlab.io.airtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtrackingApplication.class, args);
+	}
+
+}
---
+++ airtracking/src/test/java/https/airtracking/gitlab/io/airtracking/AirtrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package https.airtracking.gitlab.io.airtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirtrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
