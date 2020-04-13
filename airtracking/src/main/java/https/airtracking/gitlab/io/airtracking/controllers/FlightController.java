62fd71294f299bcba329d0450b9ad8982e3c44ab
Diogo Mota <fc46410@alunos.fc.ul.pt>
2020-04-13 17:48:22 +0100

    flightcontroller update

--- airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/controllers/FlightController.java
+++ airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/controllers/FlightController.java
@@ -5,15 +5,51 @@
  */
 package https.airtracking.gitlab.io.airtracking.controllers;
 
+import https.airtracking.gitlab.io.airtracking.FlightStateService;
+import https.airtracking.gitlab.io.airtracking.Models.FlightState;
+import https.airtracking.gitlab.io.airtracking.Models.FlightStateMessage;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/flight")
 public class FlightController {
+    
+        @Autowired
+        private FlightStateService fService;
+        
+        @GetMapping(value = "/")
+        public List<FlightStateMessage> findAll(){
+            return fService.findAll();         
+        }
+    
+        @GetMapping(value = "/{time}")
+        public FlightStateMessage findByTime(@PathVariable("time") int time){
+            return fService.findByTime(time);
+        }
+    
+        @GetMapping(value = "/states/{time}")
+        public List<FlightState> getFlightStatesByTime(@PathVariable("time") int time){
+            return fService.getFlightStatesByTime(time);
+        }
 
-	@GetMapping("/flight")
+        @PostMapping(value = "/")
+        public ResponseEntity<?> saveOrUpdateFlightStateMessage(@RequestBody FlightStateMessage message){
+            fService.saveOrUpdateFlightStateMessage(message);
+            return new ResponseEntity("Flight added successfully", HttpStatus.OK); 
+        }
+
+	/*@GetMapping("/flight")
 	public String flight(Model model) {
 		return "flight.html";
 	}
@@ -21,6 +57,6 @@ public class FlightController {
         @GetMapping("/flight/data")
 	public String flightData(Model model) {
 		return "flightData.html";
-	}
+	}*/
 
 }
