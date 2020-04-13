f36c958072f7acce5315fa9b323e7ffbcc63e578
Diogo Mota <fc46410@alunos.fc.ul.pt>
2020-04-13 19:51:08 +0100

    webapp added

---
+++ airtrackingwebapp/src/main/java/https/airtracking/gitlab/io/airtrackingwebapp/controllers/HomepageController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package https.airtracking.gitlab.io.airtrackingwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomepageController {
+
+	@GetMapping("/homepage")
+	public String homepage(Model model) {
+		return "homepage.html";
+	}
+
+}
