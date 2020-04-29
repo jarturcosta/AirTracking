2845e95cc29432257cd02b64a008a6ac4fbda088
al64145 <al64145@utad.eu>
2020-04-29 18:12:22 +0100

    map controller test

--- airtrackingwebapp/src/main/java/https/airtracking/gitlab/io/airtrackingwebapp/services/FlightState.java
+++ airtrackingwebapp/src/main/java/https/airtracking/gitlab/io/airtrackingwebapp/services/FlightState.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package services;
+package https.airtracking.gitlab.io.airtrackingwebapp.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
--- airtrackingwebapp/src/main/java/https/airtracking/gitlab/io/airtrackingwebapp/services/RestService.java
+++ airtrackingwebapp/src/main/java/https/airtracking/gitlab/io/airtrackingwebapp/services/RestService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package services;
+package https.airtracking.gitlab.io.airtrackingwebapp.services;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
