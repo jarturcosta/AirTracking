51b699707f39d337b3197ff184051ee4ad8a1377
Diogo Alves <empty.block@gmail.com>
2020-05-18 22:21:34 +0100

    [FIX] enviroment params

--- airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/KafkaFlightStateConsumer.java
+++ airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/KafkaFlightStateConsumer.java
@@ -2,9 +2,8 @@
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
-*/
+ */
 package https.airtracking.gitlab.io.airtracking;
-
 
 import https.airtracking.gitlab.io.airtracking.Models.FlightStateMessage;
 import org.apache.kafka.clients.consumer.*;
@@ -12,58 +11,53 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-
 import java.util.Collections;
 import java.util.Properties;
-/**
- *
- * @author jarturcosta
- */
-public class KafkaFlightStateConsumer extends Thread {
-    private final static String TOPIC = "ESP52-test2";
-    private final static String BOOTSTRAP_SERVERS =
-            "localhost:9092";
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
-    public KafkaFlightStateConsumer() {
-    }
-    
-    
-    
-    private static Consumer<Long, String> createConsumer() {
+@Configuration
+public class KafkaFlightStateConsumer extends Thread {
+
+    @Value("${kafka.server}")
+    private String BOOTSTRAP_SERVERS;
+    private String TOPIC = "ESP52-test2";
+
+    private Consumer<Long, String> createConsumer() {
         final Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                BOOTSTRAP_SERVERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,
-                "KafkaExampleConsumer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                LongDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        
         // Create the consumer using props.
-        final Consumer<Long, String> consumer =
-                new KafkaConsumer<>(props);
-        // Subscribe to the topic.
+        final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Collections.singletonList(TOPIC));
+
         return consumer;
     }
-    
+
     @Override
     public void run() {
         final Consumer<Long, String> consumer = createConsumer();
-        final int giveUp = 100;   int noRecordsCount = 0;
+        final int giveUp = 100;
+        int noRecordsCount = 0;
         while (true) {
-            final ConsumerRecords<Long, String> consumerRecords =
-                    consumer.poll(1000);
-            if (consumerRecords.count()==0) {
+            final ConsumerRecords<Long, String> consumerRecords
+                    = consumer.poll(1000);
+            if (consumerRecords.count() == 0) {
                 noRecordsCount++;
-                if (noRecordsCount > giveUp) break;
-                else continue;
+                if (noRecordsCount > giveUp) {
+                    break;
+                } else {
+                    continue;
+                }
             }
             consumerRecords.forEach(record -> {
                 System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                    record.key(), record.value(),
-                    record.partition(), record.offset());
-                if (record.value()!=null) {
+                        record.key(), record.value(),
+                        record.partition(), record.offset());
+                if (record.value() != null) {
                     FlightStateMessage fsm = FlightStateDeserializer.deserialize(record.value());
                     System.out.println(fsm.toString());
                 }
@@ -73,7 +67,4 @@ public class KafkaFlightStateConsumer extends Thread {
         consumer.close();
         System.out.println("DONE");
     }
-    
-    
-    
 }
--- airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/KafkaFlightStateProducer.java
+++ airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/KafkaFlightStateProducer.java
@@ -1,8 +1,3 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 package https.airtracking.gitlab.io.airtracking;
 
 import https.airtracking.gitlab.io.airtracking.Models.FlightStateMessage;
@@ -12,54 +7,42 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
-/**
- *
- * @author jarturcosta
- */
 public class KafkaFlightStateProducer extends Thread {
-    
-    private final KafkaProducer producer;
+
+    private KafkaProducer producer;
     private String topic;
     private Boolean isAsync;
-    public static final String KAFKA_SERVER_URL = "localhost";
-    public static final int KAFKA_SERVER_PORT = 9092;
     public static final String CLIENT_ID = "SampleProducer";
-    
-    @Autowired
-    private FlightStateMessageController controller;
-    
-    public KafkaFlightStateProducer(String topic, Boolean isAsync) {
+
+    public KafkaFlightStateProducer(String kafka_server, String topic, Boolean isAsync) {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", KAFKA_SERVER_URL + ":" + KAFKA_SERVER_PORT);
+        properties.put("bootstrap.servers", kafka_server);
         properties.put("client.id", CLIENT_ID);
         properties.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        producer = new KafkaProducer(properties);
+        this.producer = new KafkaProducer(properties);
         this.topic = topic;
         this.isAsync = isAsync;
     }
+
     public void run() {
         int messageNo = 1;
         while (true) {
@@ -71,12 +54,12 @@ public class KafkaFlightStateProducer extends Thread {
             }
             long startTime = System.currentTimeMillis();
             if (isAsync) { // Send asynchronously
-                producer.send(new ProducerRecord(topic,
+                this.producer.send(new ProducerRecord(topic,
                         messageNo,
                         messageStr), new DemoCallBack(startTime, messageNo, messageStr));
             } else { // Send synchronously
                 try {
-                    producer.send(new ProducerRecord(topic,
+                    this.producer.send(new ProducerRecord(topic,
                             messageNo,
                             messageStr)).get();
                     FlightStateMessage fsm = FlightStateDeserializer.deserialize(messageStr);
@@ -87,10 +70,9 @@ public class KafkaFlightStateProducer extends Thread {
                         System.out.println("ERROR: " + e.toString());
                         System.out.println("FSM: " + fsm.toString2());
                     }
-                    
-                    
+
                     System.out.println("Saved in BD! Time:" + fsm.getTime());
-                    
+
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
                     // handle the exception
@@ -99,7 +81,7 @@ public class KafkaFlightStateProducer extends Thread {
             ++messageNo;
         }
     }
-    
+
     private void sendPost(String body) throws Exception {
 
         HttpPost post = new HttpPost("http://192.168.160.103:9069/flightstates/");
@@ -110,20 +92,20 @@ public class KafkaFlightStateProducer extends Thread {
         post.setEntity(entity);
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault();
-             CloseableHttpResponse response = httpClient.execute(post)) {
+                CloseableHttpResponse response = httpClient.execute(post)) {
 
             System.out.println(EntityUtils.toString(response.getEntity()));
         }
 
     }
-    
+
     public static String getAllStates() throws MalformedURLException, IOException {
         URL url = new URL("https://opensky-network.org/api/states/all");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        
+
         int status = con.getResponseCode();
-        
+
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -132,37 +114,42 @@ public class KafkaFlightStateProducer extends Thread {
             content.append(inputLine);
         }
         in.close();
-        
+
         return content.toString();
     }
-    
-    
-class DemoCallBack implements Callback {
-    private final long startTime;
-    private final int key;
-    private final String message;
-    public DemoCallBack(long startTime, int key, String message) {
-        this.startTime = startTime;
-        this.key = key;
-        this.message = message;
-    }
-    /**
-     * onCompletion method will be called when the record sent to the Kafka Server has been acknowledged.
-     *
-     * @param metadata  The metadata contains the partition and offset of the record. Null if an error occurred.
-     * @param exception The exception thrown during processing of this record. Null if no error occurred.
-     */
-    public void onCompletion(RecordMetadata metadata, Exception exception) {
-        long elapsedTime = System.currentTimeMillis() - startTime;
-        if (metadata != null) {
-            System.out.println(
-                    "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
-                    "), " +
-                    "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
-        } else {
-            exception.printStackTrace();
+
+    class DemoCallBack implements Callback {
+
+        private final long startTime;
+        private final int key;
+        private final String message;
+
+        public DemoCallBack(long startTime, int key, String message) {
+            this.startTime = startTime;
+            this.key = key;
+            this.message = message;
+        }
+
+        /**
+         * onCompletion method will be called when the record sent to the Kafka
+         * Server has been acknowledged.
+         *
+         * @param metadata The metadata contains the partition and offset of the
+         * record. Null if an error occurred.
+         * @param exception The exception thrown during processing of this
+         * record. Null if no error occurred.
+         */
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            if (metadata != null) {
+                System.out.println(
+                        "message(" + key + ", " + message + ") sent to partition(" + metadata.partition()
+                        + "), "
+                        + "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
+            } else {
+                exception.printStackTrace();
+            }
         }
     }
-}
-    
+
 }
