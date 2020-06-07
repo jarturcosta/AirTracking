0c29f0e4b2dd61d2a6ff74c510267049d0f7906f
Diogo Alves <empty.block@gmail.com>
2020-06-07 22:08:50 +0100

    [ADD] cross origin

--- airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/FlightStateMessageController.java
+++ airtracking/src/main/java/https/airtracking/gitlab/io/airtracking/FlightStateMessageController.java
@@ -39,7 +39,7 @@ import org.springframework.data.mongodb.repository.Query;
  */
 @RestController
 @RequestMapping("/flightstates")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = {"http://localhost:8080", "http://192.168.160.103:9070"})
 public class FlightStateMessageController {
     
     @Autowired
